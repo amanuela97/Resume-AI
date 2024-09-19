e6405e296eecb90166e3288a6d5abaee5929c1fb
amanuela97 <babyzewdie@gmail.com>
2024-09-19 17:51:31 +0300

    fix: use scrape api instead of puppeteer

--- app/api/extract/route.ts
+++ app/api/extract/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import { z } from "zod";
 import * as cheerio from "cheerio";
-import puppeteer, { Browser } from "puppeteer-core";
-import chromium from "@sparticuz/chromium-min";
+import fetch from "node-fetch";
 
 const urlSchema = z.string().url();
 
@@ -17,46 +16,12 @@ export async function POST(request: Request) {
   }
 
   try {
-    const isLocal = process.env.LOCAL_ENV;
-    let browser: Browser;
-    if (isLocal) {
-      // if we are running locally, use the puppeteer that is installed in the node_modules folder
-      browser = await require("puppeteer").launch();
-    } else {
-      // if we are running in AWS, download and use a compatible version of chromium at runtime
-      browser = await puppeteer.launch({
-        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-        executablePath: await chromium.executablePath(
-          `https://github.com/Sparticuz/chromium/releases/download/v127.0.0/chromium-v127.0.0-pack.tar`
-        ),
-        headless: true,
-        ignoreHTTPSErrors: true,
-      });
-    }
-    const timeoutPromise = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error("Request timed out")), 9000)
+    const response = await fetch(
+      `http://api.scraperapi.com?api_key=${process.env.SCRAPE_API_KEY}&url=${url}`
     );
+    const html = await response.text();
 
-    const page = await browser.newPage();
-    console.log(await page.browser().version());
-    const fetchDataPromise = page.goto(url, {
-      waitUntil: "domcontentloaded",
-      timeout: 10000,
-    });
-    await Promise.race([timeoutPromise, fetchDataPromise]);
-    await page.setRequestInterception(true);
-    page.on("request", (req) => {
-      if (["image", "stylesheet", "font"].includes(req.resourceType())) {
-        req.abort();
-      } else {
-        req.continue();
-      }
-    });
-
-    const content = await page.content();
-    await browser.close();
-
-    const $ = cheerio.load(content);
+    const $ = cheerio.load(html);
     let text = $("body").text();
     text = text.replace(/\s+/g, " ").trim();
     text = text.substring(0, 2800); // Extract up to 2800 characters
